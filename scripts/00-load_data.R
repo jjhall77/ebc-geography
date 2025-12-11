e5e64468ba880dd57686e9726b1b8137b0472b97
John Hall <johnhall@Mac.fios-router.home>
2025-12-11 13:23:01 -0500

    practice

---
+++ scripts/00-load_data.R
@@ -0,0 +1,17 @@
+library(here)
+library(tidyverse)
+library(sf)
+library(janitor)
+
+list.files(here("data/lion"))
+
+# Path to the geodatabase
+lion_gdb <- here("data", "lion", "lion.gdb")
+
+# List available layers inside the .gdb
+st_layers(lion_gdb)
+
+lion_gdb <- here("data", "lion", "lion.gdb")
+
+lion <- st_read(lion_gdb, layer = "lion") %>%
+  st_transform(2263)   # NYC coordinates, ft
